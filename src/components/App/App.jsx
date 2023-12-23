2d6fb87113f9af80fd6889d32225fd3eb7ccefbf
a1adikk-07 <136816822+a1adikk-07@users.noreply.github.com>
2023-12-23 16:51:03 +0200

    task3

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -10,6 +10,7 @@ import {
 
 import article from 'data/article.json'
 import stats from 'data/data.json'
+import list from 'data/forbes.json'
 
 export const App = () => {
   return (
@@ -28,7 +29,7 @@ export const App = () => {
         <Heading marginTop="50px" marginBottom="50px" textAlign="center">
           Task 3
         </Heading>
-        <ForbesList />
+        <ForbesList list={ list } />
 
         <Heading marginTop="50px" marginBottom="50px" textAlign="center">
           Task 4
