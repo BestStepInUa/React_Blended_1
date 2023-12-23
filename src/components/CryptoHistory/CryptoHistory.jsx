7de01c42b02bdd42ca25f64fc14d04b9a3d35653
BestStepInUa <beststep.in.ua@gmail.com>
2023-12-23 17:03:47 +0200

    unused components fix

--- src/components/CryptoHistory/CryptoHistory.jsx
+++ src/components/CryptoHistory/CryptoHistory.jsx
@@ -1,4 +1,4 @@
-import { BaseTable, THead, Th, Tr, Td } from './CryptoHistory.styled';
+// import { BaseTable, THead, Th, Tr, Td } from './CryptoHistory.styled';
 
 export const CryptoHistory = () => {
   return <div>CryptoHistory</div>;
